16991f0fdd13c47613ff0b375300775d5af1d7f7
Candice Persil <candice.persil@gmail.com>
2024-02-25 18:14:07 +0100

    Create basic roots

--- main.py
+++ main.py
@@ -29,6 +29,32 @@ def read_simple_root() -> dict[str, str]:
     return {"content": "Here is the simplest root ever!"}
 
 
+@app.get("/movements/", response_model=list[workout_shema.Movement])
+def get_movements(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    movements = queries.get_movements(db, skip=skip, limit=limit)
+    return movements
+
+
+@app.post("/movements/", response_model=workout_shema.Movement)
+def create_movement(
+    movement: workout_shema.MovementCreate, db: Session = Depends(get_db)
+) -> dict[str, Any]:
+    return operations.create_movement(db=db, movement=movement)
+
+
+@app.get("/exercises/", response_model=list[workout_shema.Exercise])
+def get_exercises(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    exercises = queries.get_exercises(db, skip=skip, limit=limit)
+    return exercises
+
+
+@app.post("/exercises/", response_model=workout_shema.Exercise)
+def create_exercise(
+    exercise: workout_shema.ExerciseCreate, db: Session = Depends(get_db)
+) -> dict[str, Any]:
+    return operations.create_exercise(db=db, exercise=exercise)
+
+
 @app.get("/workouts/{date}", response_model=workout_shema.Workout)
 def get_workout(date: Union[date, None] = None, db: Session = Depends(get_db)):
     db_workout = queries.get_workout_by_date(db=db, date=date)
@@ -48,7 +74,7 @@ def get_workouts(skip: int = 0, limit: int = 100, db: Session = Depends(get_db))
 
 @app.post("/workouts/", response_model=workout_shema.Workout)
 def create_workout(
-    workout: workout_shema.Workout, db: Session = Depends(get_db)
+    workout: workout_shema.WorkoutCreate, db: Session = Depends(get_db)
 ) -> dict[str, Any]:
     db_workout = queries.get_workout_by_date(db, date=workout.date)
     if db_workout:
